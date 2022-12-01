68b46368cb59d24860f5c5121e6a982c9e7c1a5b
Maarten Oosterhoff <maarten.oosterhoff@gmail.com>
2022-12-01 16:21:51 +0100

    🎨 Adds file-scoped namespaces.

--- src/AdventOfCode2022.Puzzles/AdventOfCode2022.Puzzles/Puzzle1/Runner.cs
+++ src/AdventOfCode2022.Puzzles/AdventOfCode2022.Puzzles/Puzzle1/Runner.cs
@@ -2,58 +2,57 @@
 using FluentAssertions;
 using Xunit;
 
-namespace AdventOfCode2022.Puzzles.Puzzle1
+namespace AdventOfCode2022.Puzzles.Puzzle1;
+
+public class Runner
 {
-    public class Runner
+    [Theory]
+    [InlineData("TestInput", 24000L)]
+    [InlineData("Input", 69912L)]
+    public void RunAlpha(string filename, long expected)
     {
-        [Theory]
-        [InlineData("TestInput", 24000L)]
-        [InlineData("Input", 69912L)]
-        public void RunAlpha(string filename, long expected)
+        var actual = Execute(filename).MaxBy(x => x.calorieTotal).calorieTotal;
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("TestInput", 45000L)]
+    [InlineData("Input", 208180L)]
+    public void RunBeta(string filename, long expected)
+    {
+        var actual = Execute(filename)
+            .OrderByDescending(x => x.calorieTotal)
+            .Take(3)
+            .Select(x => x.calorieTotal)
+            .Sum();
+
+        actual.Should().Be(expected);
+    }
+
+    private static IEnumerable<(int elfId, long calorieTotal)> Execute(string filename)
+    {
+        var lines = EmbeddedResourceReader.Read("Puzzle1", filename)
+            .Replace(Environment.NewLine, "\n")
+            .Replace("\r", "\n")
+            .Split("\n");
+        List<(int elfId, List<long> calories)> data = new();
+
+        (int elfId, List<long> calories) current = (1, new List<long>());
+        foreach (var line in lines)
         {
-            var actual = Execute(filename).MaxBy(x => x.calorieTotal).calorieTotal;
-            actual.Should().Be(expected);
-        }
-
-        [Theory]
-        [InlineData("TestInput", 45000L)]
-        [InlineData("Input", 208180L)]
-        public void RunBeta(string filename, long expected)
-        {
-            var actual = Execute(filename)
-                .OrderByDescending(x => x.calorieTotal)
-                .Take(3)
-                .Select(x => x.calorieTotal)
-                .Sum();
-
-            actual.Should().Be(expected);
-        }
-
-        private static IEnumerable<(int elfId, long calorieTotal)> Execute(string filename)
-        {
-            var lines = EmbeddedResourceReader.Read("Puzzle1", filename)
-                .Replace(Environment.NewLine, "\n")
-                .Replace("\r", "\n")
-                .Split("\n");
-            List<(int elfId, List<long> calories)> data = new();
-
-            (int elfId, List<long> calories) current = (1, new List<long>());
-            foreach (var line in lines)
+            if (string.IsNullOrEmpty(line))
             {
-                if (string.IsNullOrEmpty(line))
-                {
-                    data.Add(current);
-                    current = (current.elfId + 1, new List<long>());
-                }
-                else
-                {
-                    current.calories.Add(long.Parse(line));
-                }
+                data.Add(current);
+                current = (current.elfId + 1, new List<long>());
             }
-
-            data.Add(current);
-
-            return data.Select(x => (x.elfId, x.calories.Sum()));
+            else
+            {
+                current.calories.Add(long.Parse(line));
+            }
         }
+
+        data.Add(current);
+
+        return data.Select(x => (x.elfId, x.calories.Sum()));
     }
 }
--- src/AdventOfCode2022.Puzzles/AdventOfCode2022.Puzzles/Utils/EmbeddedResourceReader.cs
+++ src/AdventOfCode2022.Puzzles/AdventOfCode2022.Puzzles/Utils/EmbeddedResourceReader.cs
@@ -1,18 +1,17 @@
 ﻿using System.Reflection;
 
-namespace AdventOfCode2022.Puzzles.Utils
-{
-    internal class EmbeddedResourceReader
-    {
-        public static string Read(string folder, string filename)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = $"{typeof(EmbeddedResourceReader).Namespace?.Replace(".Utils", "")}.{folder}.{filename}.txt";
+namespace AdventOfCode2022.Puzzles.Utils;
 
-            using var stream = assembly.GetManifestResourceStream(resourceName);
-            using var reader = new StreamReader(stream!);
-            var result = reader.ReadToEnd();
-            return result;
-        }
+internal class EmbeddedResourceReader
+{
+    public static string Read(string folder, string filename)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = $"{typeof(EmbeddedResourceReader).Namespace?.Replace(".Utils", "")}.{folder}.{filename}.txt";
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        using var reader = new StreamReader(stream!);
+        var result = reader.ReadToEnd();
+        return result;
     }
 }
